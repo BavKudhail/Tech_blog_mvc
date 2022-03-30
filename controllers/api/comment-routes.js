d83aec433ecc2465ca1e09483eb254b9a481d8a8
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-03-30 12:41:41 +0100

    user, comment, post routes have base logic all working

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,4 +1,35 @@
 const router = require("express").Router();
-const { Comment } = require("../../models");
+const { Comment, User } = require("../../models");
+
+// api/comment
+
+// GET ALL COMMENTS
+router.get("/", async (req, res) => {
+  try {
+    //   find all comments
+    const commentData = await Comment.findAll({
+      // include the user associated with the comment
+      include: [User],
+    });
+    // serialize the data
+    const comments = commentData.map((comment) => comment.get({ plain: true }));
+    // display comments
+    res.json(comments);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// CREATE A NEW COMMENT
+router.post("/", async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      ...req.body,
+    });
+    res.json(newComment);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,4 +1,38 @@
 const router = require("express").Router();
+const { User } = require("../../models");
+
+// api/user
+
+// @TODO - THIS REQUEST IS FOR DEV PURPOSES ONLY
+router.get("/", async (req, res) => {
+  try {
+    //   get all users
+    const userData = await User.findAll();
+    // serialize the data
+    const users = userData.map((user) => user.get({ plain: true }));
+    // render the data
+    res.json(users);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+// SIGN UP
+router.post("/", async (req, res) => {
+  try {
+    const newUser = await User.create({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    res.json(newUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// LOGIN
+
+// LOGOUT
 
 router.get("/", (req, res) => {
   res.send("You are now on API/USERS");
